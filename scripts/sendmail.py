9d15a5a229ec41685ee197907af726ce752025b8
IsakTheHacker <isak.brynielsson@gmail.com>
2021-02-18 22:06:37 +0100

    Fix an encoding issue.

--- scripts/sendmail.py
+++ scripts/sendmail.py
@@ -20,7 +20,7 @@ Subject: {}\n\
 server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 server.ehlo()
 server.login(gmail_user, gmail_password)
-server.sendmail(gmail_user, to, email_text)
+server.sendmail(gmail_user, to, email_text.encode("utf-8"))
 server.close()
 
 print('Email sent!')
