aa9c6f45dea8d63dcbe4c97ee0e849388de9335c
IsakTheHacker <isak.brynielsson@gmail.com>
2021-03-06 00:14:12 +0100

    Add basic error handling for cases when the menu isn't found.

--- scripts/rssgetmenu.py
+++ scripts/rssgetmenu.py
@@ -1,5 +1,10 @@
 import feedparser
+import sys
 
 def getmenu():
-    NewsFeed = feedparser.parse("https://skolmaten.se/nya-elementar/rss/days/")
-    return NewsFeed.entries[0].summary.split("<br />")
+	NewsFeed = feedparser.parse("https://skolmaten.se/nya-elementar/rss/days/")
+	try:
+		return NewsFeed.entries[0].summary.split("<br />")
+	except IndexError:
+		print("Matsedeln kunde inte hittas!")
+		sys.exit(1)
